de83ff5b3fe6e5d67440177fa6aca3ca9d8b0c0a
Carlos Pontón <carlitosponton@hotmail.com>
2022-12-02 20:23:38 +0100

    Add knex file

---
+++ knexfile.ts
@@ -0,0 +1,20 @@
+import knex, { Knex } from 'knex'
+import path from 'path'
+import { getKnexConfig } from './persistence/db'
+
+const knexConfig: Knex.Config = {
+  client: 'mysql',
+  connection: getKnexConfig(),
+  pool: {
+    min: 2,
+    max: 10,
+  },
+  seeds: {
+    directory: path.join(process.cwd(), 'seeders')
+  },
+  migrations: {
+    directory: path.join(process.cwd(), 'migrations'),
+  },
+}
+
+module.exports = knexConfig

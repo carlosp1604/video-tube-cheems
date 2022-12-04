a0a0ce8cfefd0afc033f6dbe1cd89f58faf3fef1
Carlos Pontón <carlitosponton@hotmail.com>
2022-12-04 20:50:51 +0100

    Add  migration to create posts_meta table

---
+++ migrations/20221204182755_create_posts_meta_table.ts
@@ -0,0 +1,26 @@
+import { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema
+    .createTable('posts_meta', function (table) {
+      table.string('type', 64).notNullable()
+      table.string('value', 512).notNullable()
+      table.primary(['posts_id', 'type'])
+      table.string('posts_id', 36)
+        .references('id')
+        .inTable('posts')
+        .notNullable()
+      table.timestamp('created_at')
+        .defaultTo(knex.raw('CURRENT_TIMESTAMP'))
+        .notNullable()
+      table.timestamp('updated_at')
+        .defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))
+        .notNullable()
+    })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('posts_meta')
+}
+

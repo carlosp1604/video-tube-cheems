1082264812c5e6713d5eba13e55363c0254af453
Carlos Pontón <carlitosponton@hotmail.com>
2022-12-07 00:08:42 +0100

    Implement a new method (findWithOffsetAndLimit) in the post repository

--- modules/Posts/Domain/PostRepositoryInterface.ts
+++ modules/Posts/Domain/PostRepositoryInterface.ts
@@ -1,7 +1,8 @@
 import { Post } from './Post'
 
 export type RepositoryOptions = 'meta' | 'tags' | 'actors'
-
+export type RepositoryOrderCriteria = 'date_asc' | 'date_desc' | 'views_asc' | 'views_desc'
+export type RepositoryFilter = { type: 'title', value: string }
 export interface PostRepositoryInterface {
   /**
    * Insert a Post in the persistence layer
@@ -15,4 +16,19 @@ export interface PostRepositoryInterface {
    * @return Post if found or null
    */
   findById(postId: Post['id']): Promise<Post | null>
+
+  /**
+   * Find Posts based on filter and order criteria
+   * @param offset Post offset
+   * @param limit
+   * @param order Post sorting criteria
+   * @param filter Post filter
+   * @return Post if found or null
+   */
+  findWithOffsetAndLimit(
+    offset: number,
+    limit: number,
+    order: RepositoryOrderCriteria,
+    filter?: RepositoryFilter
+  ): Promise<Post[]>
 }
--- modules/Posts/Infrastructure/MysqlPostRepository.ts
+++ modules/Posts/Infrastructure/MysqlPostRepository.ts
@@ -1,4 +1,4 @@
-import { PostRepositoryInterface } from '../Domain/PostRepositoryInterface'
+import { PostRepositoryInterface, RepositoryFilter, RepositoryOrderCriteria } from '../Domain/PostRepositoryInterface'
 import { Post } from '../Domain/Post'
 import knex from 'knex'
 import { Model } from 'objection'
@@ -21,6 +21,7 @@ export class MysqlPostRepository implements PostRepositoryInterface {
    * @return Post if found or null
    */
   public async findById(postId: Post['id']): Promise<Post | null> {
+    // TODO: Find a solution for this
     const knexInstance = knex(knexConfig)
     Model.knex(knexInstance)
 
@@ -35,5 +36,57 @@ export class MysqlPostRepository implements PostRepositoryInterface {
     }
 
     return PostModelTranslator.toDomain(posts[0], ['meta', 'tags', 'actors'])
+  }
+
+  /**
+   * Find Posts based on filter and order criteria
+   * @param offset Post offset
+   * @param limit
+   * @param order Post sorting criteria
+   * @param filter Post filter
+   * @return Post if found or null
+   */
+  public async findWithOffsetAndLimit(
+    offset: number,
+    limit: number,
+    order: RepositoryOrderCriteria = 'date_desc',
+    filter?: RepositoryFilter
+  ): Promise<Post[]> {
+    // TODO: Find a solution for this
+    const knexInstance = knex(knexConfig)
+    Model.knex(knexInstance)
+
+    const posts = await ObjectionPostModel.query()
+      .withGraphFetched('meta')
+      .withGraphFetched('actors')
+      .limit(limit)
+      .offset(offset)
+      .whereNotNull('published_at')
+      .andWhere((builder) => {
+        if (order === 'date_desc') {
+          builder.orderBy('published_at', 'desc')
+        }
+
+        if (order === 'date_asc') {
+          builder.orderBy('published_at', 'asc')
+        }
+
+        if (order === 'views_asc') {
+          builder.orderBy('views_count', 'asc')
+        }
+
+        if (order === 'views_desc') {
+          builder.orderBy('views_count', 'desc')
+        }
+
+        if (filter) {
+          // NOTE: At the moment we only support filter by post title
+          builder.where('title', 'like', `%${filter.value}%`)
+        }
+      })
+
+      return posts.map((post) =>
+        PostModelTranslator.toDomain(post, ['meta', 'actors']))
+
   }
 }

26af687a78656c0536f3cdfbe154fe63dcfcb07d
Carlos Pontón <carlitosponton@hotmail.com>
2022-12-06 22:23:13 +0100

    Refactor objection post model to include the relationships with actors and post_tags

--- modules/Posts/Infrastructure/ObjectionPostModel.ts
+++ modules/Posts/Infrastructure/ObjectionPostModel.ts
@@ -1,5 +1,7 @@
 import { Model } from 'objection'
 import { ObjectionPostMetaModel } from './ObjectionPostMetaModel'
+import { ObjectionPostTagModel } from './ObjectionPostTagModel'
+import { ObjectionActorModel } from './ObjectionActorModel'
 
 export class ObjectionPostModel extends Model {
   id!: string
@@ -10,6 +12,8 @@ export class ObjectionPostModel extends Model {
   updated_at!: Date
   deleted_at!: Date | null
   meta!: ObjectionPostMetaModel[]
+  tags!: ObjectionPostTagModel[]
+  actors!: ObjectionActorModel[]
 
   public static get tableName(): string {
     return 'posts'
@@ -22,6 +26,30 @@ export class ObjectionPostModel extends Model {
       join: {
         from: 'posts.id',
         to: 'posts_meta.post_id'
+      }
+    },
+    tags: {
+      relation: Model.ManyToManyRelation,
+      modelClass: ObjectionPostTagModel,
+      join: {
+        from: 'posts.id',
+        through: {
+          from: 'post_post_tags.post_id',
+          to: 'post_post_tags.post_tag_id'
+        },
+        to: 'post_tags.id',
+      }
+    },
+    actors: {
+      relation: Model.ManyToManyRelation,
+      modelClass: ObjectionActorModel,
+      join: {
+        from: 'posts.id',
+        through: {
+          from: 'post_actors.post_id',
+          to: 'post_actors.actor_id'
+        },
+        to: 'actors.id',
       }
     }
   }

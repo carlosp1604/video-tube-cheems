1118ee81b62e934a84cf21ab10cdfb599777c90f
Carlos Pontón <carlitosponton@hotmail.com>
2022-12-06 22:24:03 +0100

    Load the actos and post_tags relationships

--- modules/Posts/Infrastructure/PostModelTranslator.ts
+++ modules/Posts/Infrastructure/PostModelTranslator.ts
@@ -2,6 +2,8 @@ import { ObjectionPostModel } from './ObjectionPostModel'
 import { RepositoryOptions } from '../Domain/PostRepositoryInterface'
 import { Post } from '../Domain/Post'
 import { PostMetaModelTranslator } from './PostMetaModelTranslator'
+import { PostTagModelTranslator } from './PostTagModelTranslator'
+import { ActorModelTranslator } from './ActorModelTranslator'
 
 export class PostModelTranslator {
   public static toDomain(
@@ -18,10 +20,24 @@ export class PostModelTranslator {
       objectionPostModel.deleted_at
     )
 
-    if (options.includes('postMeta')) {
+    if (options.includes('meta')) {
       for (let i = 0; i < objectionPostModel.meta.length; i++) {
         const postMetaDomain = PostMetaModelTranslator.toDomain(objectionPostModel.meta[i])
         post.addMeta(postMetaDomain)
+      }
+    }
+
+    if (options.includes('tags')) {
+      for (let i = 0; i < objectionPostModel.tags.length; i++) {
+        const postTagDomain = PostTagModelTranslator.toDomain(objectionPostModel.tags[i])
+        post.addTag(postTagDomain)
+      }
+    }
+
+    if (options.includes('actors')) {
+      for (let i = 0; i < objectionPostModel.actors.length; i++) {
+        const actorDomain = ActorModelTranslator.toDomain(objectionPostModel.actors[i])
+        post.addActor(actorDomain)
       }
     }
 

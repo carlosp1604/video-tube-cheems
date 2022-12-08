e19950591c09d814d0017b26bd057e805735537b
Carlos Pontón <carlitosponton@hotmail.com>
2022-12-08 17:13:18 +0100

    Add migration to create users table

---
+++ migrations/20221207221317_create_user_table.ts
@@ -0,0 +1,27 @@
+import { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema
+    .createTable('users', function (table) {
+      table.string('id', 36).primary().notNullable()
+      table.string('name', 256).notNullable()
+      table.string('email', 256).notNullable().unique()
+      table.string('image_url', 256).nullable()
+      table.string('language', 16).notNullable()
+      table.timestamp('email_verified').nullable()
+      table.integer('views_count').notNullable().defaultTo(0)
+      table.timestamp('created_at')
+        .defaultTo(knex.raw('CURRENT_TIMESTAMP'))
+        .notNullable()
+      table.timestamp('updated_at')
+        .defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))
+        .notNullable()
+      table.timestamp('deleted_at')
+        .defaultTo(null)
+    })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('users')
+}
+

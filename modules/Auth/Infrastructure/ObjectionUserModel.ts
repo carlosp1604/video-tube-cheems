6f3290211f0f0314bbcbf9328e626ddaaac55c19
Carlos Pontón <carlitosponton@hotmail.com>
2022-12-08 17:14:39 +0100

    Create the objection user model and its translator

---
+++ modules/Auth/Infrastructure/ObjectionUserModel.ts
@@ -0,0 +1,18 @@
+import { Model } from 'objection'
+
+export class ObjectionUserModel extends Model {
+  id!: string
+  name!: string
+  email!: string
+  views_count!: number
+  image_url!: string | null
+  language!: string
+  created_at!: Date
+  updated_at!: Date
+  deleted_at!: Date | null
+  email_verified!: Date | null
+
+  public static get tableName(): string {
+    return 'users'
+  }
+}
---
+++ modules/Auth/Infrastructure/UserModelTranslator.ts
@@ -0,0 +1,49 @@
+import { DateTime } from 'luxon'
+import { ObjectionUserModel } from './ObjectionUserModel'
+import { User } from '../Domain/User'
+import { ModelObject } from 'objection'
+
+type MysqlUserRow = ModelObject<ObjectionUserModel>
+
+export class UserModelTranslator {
+  public static toDomain(objectionUserModel: ObjectionUserModel) {
+    let deletedAt: DateTime | null = null
+    let emailVerifiedAt: DateTime | null = null
+
+    if (objectionUserModel.email_verified !== null) {
+      emailVerifiedAt = DateTime.fromJSDate(objectionUserModel.email_verified)
+    }
+
+    if (objectionUserModel.deleted_at !== null) {
+      deletedAt = DateTime.fromJSDate(objectionUserModel.deleted_at)
+    }
+
+    return new User (
+      objectionUserModel.id,
+      objectionUserModel.name,
+      objectionUserModel.email,
+      objectionUserModel.image_url,
+      objectionUserModel.views_count,
+      objectionUserModel.language,
+      DateTime.fromJSDate(objectionUserModel.created_at),
+      DateTime.fromJSDate(objectionUserModel.updated_at),
+      emailVerifiedAt,
+      deletedAt,
+    )
+  }
+
+  public static toDatabase(user: User): MysqlUserRow {
+    return {
+      id: user.id,
+      email: user.email,
+      email_verified: user.emailVerified?.toJSDate() ?? null,
+      name: user.name,
+      created_at: user.createdAt.toJSDate(),
+      deleted_at: user.deletedAt?.toJSDate() ?? null,
+      updated_at: user.updatedAt.toJSDate(),
+      views_count: user.viewsCount,
+      image_url: user.imageUrl,
+      language: user.language
+    }
+  }
+}

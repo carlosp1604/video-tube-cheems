83ada2b91874a252af440ef880416580e589f807
Carlos Pontón <carlitosponton@hotmail.com>
2023-01-22 20:06:04 +0100

    Add webpack config and some parameters to tailwindcss config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,27 @@ const { Model } = require('objection')
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config) => {
+    const rules = config.module.rules
+        .find((rule) => typeof rule.oneOf === 'object')
+        .oneOf.filter((rule) => Array.isArray(rule.use))
+
+    rules.forEach((rule) => {
+      rule.use.forEach((moduleLoader) => {
+        if (/css-loader[/\\](?:cjs|dist|src)/.test(moduleLoader.loader)) {
+          if (typeof moduleLoader.options.modules === 'object') {
+            moduleLoader.options.modules = {
+              ...moduleLoader.options.modules,
+              // https://github.com/webpack-contrib/css-loader#exportlocalsconvention
+              exportLocalsConvention: 'dashesOnly',
+            }
+          }
+        }
+      })
+    })
+
+    return config
+  },
   i18n: {
     // These are all the locales you want to support in
     // your application
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,48 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
+  important: true,
   theme: {
-    extend: {},
+    screens: {
+      'sm': '500px',
+      'tb': '600px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    extend: {
+      colors: {
+        body: '#121212',
+        menu: {
+          main: '#a1a1aa',
+          highlighted: '#ffffff',
+          background: {
+            hovered: '#64748b',
+            active: '#94a3b8',
+          },
+        },
+        border: {
+          main: '#475569',
+        },
+        footer: {
+          main: '#6b7280',
+        },
+        texts: {
+          main: '#f5f5f5',
+        }
+      },
+      fontFamily: {
+        'roboto': ['Roboto', 'sans-serif']
+      },
+      transitionProperty: {
+        'width': 'width',
+        'border': 'border',
+        'opacity': 'opacity'
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('tailwind-scrollbar-hide')
+  ]
 }

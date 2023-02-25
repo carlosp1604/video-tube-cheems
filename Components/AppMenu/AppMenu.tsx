fc2f564f29140890c66a19c18bd59825e7603d2d
Carlos Pontón <carlitosponton@hotmail.com>
2023-02-26 00:57:02 +0100

    Add some changes to app menu

--- Components/AppMenu/AppMenu.tsx
+++ Components/AppMenu/AppMenu.tsx
@@ -1,13 +1,44 @@
-import { ChangeEvent, FC, KeyboardEvent, useState } from 'react'
+import { ChangeEvent, FC, KeyboardEvent, ReactElement, useEffect, useState } from 'react'
 import styles from './AppMenu.module.scss'
 import Link from 'next/link'
 import { CiSearch, CiUser } from 'react-icons/ci'
 import { useRouter } from 'next/router'
+import { useSession } from 'next-auth/react'
 
 export const AppMenu: FC = () => {
   const [openSearchBar, setOpenSearchBar] = useState<boolean>(false)
   const [title, setTitle] = useState<string>('')
+  const [userAvatar, setUserAvatar] = useState<ReactElement>(( 
+    <button
+      className={ styles.appMenu__menuButton }
+      onClick={async () => {
+        await router.push({
+          pathname: '/auth/signin/',
+          query: {
+            callbackUrl: `${router.pathname}`
+          },
+        })
+      }}
+    >
+      <CiUser className={ styles.appMenu__menuIcon }/>
+    </button>
+  ))
   const router = useRouter()
+  const session = useSession()
+
+  useEffect(() => {
+    if (session.status === 'authenticated') {
+      console.log('authenticated')
+      setUserAvatar(
+        <button className={styles.appMenu__userAvatarButton}>
+          <img className={styles.appMenu__userAvatarImage}
+            src={session.data.user.image ?? 'https://s.alamy.com/kdawwlsweh27/2LtummpjO849eQ83yGGiUN/b33c73279163c84b65241cdfcc1c8844/Fresh_Stock_Content.jpg?fm=jpg&q=100'}
+          />
+        </button>
+      )
+    }
+
+  }, [session.status])
 
   const handleKeyDown = async (event: KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter') {
@@ -67,12 +98,7 @@ export const AppMenu: FC = () => {
               handleChange(event)
             }}
           />
-          <Link
-            className={ styles.appMenu__menuLink }
-            href='/'
-          >
-            <CiUser className={ styles.appMenu__menuIcon }/>
-          </Link>
+          {userAvatar}
         </div>
       </div>
     </nav>

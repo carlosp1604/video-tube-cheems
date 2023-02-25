ef5e6001e10a14847350561b9375db26a1325624
Carlos Pontón <carlitosponton@hotmail.com>
2023-02-26 00:58:09 +0100

    Add working signin page

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -1,61 +1,14 @@
 import { NextPage } from 'next'
-import { signIn } from 'next-auth/react'
-import { FormEvent, useState } from 'react'
+import { Login } from '../../Components/Login/Login'
+import styles from '../../Components/pages/SigninPage/SigninPage.module.scss'
 
 const SignInPage: NextPage = () => {
-  const [email, setEmail] = useState<string>()
-  const [password, setPassword] = useState<string>()
-
-  const onSubmit = async (event: FormEvent) => {
-    event.preventDefault()
-
-    const result = await signIn('credentials', {
-      redirect: false,
-      email,
-      password,
-      callbackUrl: '/',
-    })
-  }
-
   return (
-    <>
-      <form
-        onSubmit={ onSubmit }
-      >
-        <h1
-        >
-          { 'ASDASD' }
-        </h1>
-        <label
-        >
-          { 'ALSDSO' }
-          <div
-          >
-            <input
-              type='email'
-              name='email'
-              placeholder={ 'email' }
-              onChange={ (event) => {
-                setEmail(event.target.value)
-              }}
-            />
-            <input
-              type='password'
-              name='password'
-              placeholder={ 'password' }
-              onChange={ (event) => {
-                setPassword(event.target.value)
-              }}
-            />
-          </div>
-        </label>
-        <button
-          data-qa='sign-in-button'
-        >
-          {'SigIn'}
-        </button>
-      </form>
-    </>
+    <section className={styles.signinPage__container}>
+      <div className={styles.signinPage__login}>
+        <Login />
+      </div>
+    </section>
   )
 }
 

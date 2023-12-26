21339cf171de032c5a9fe179ab0b79bf0fb88644
Carlos Pontón <carlitosponton@hotmail.com>
2023-12-26 21:44:49 +0100

    Fix bug in the tooltip component
    Former-commit-id: e05ef10a559d7131295fffb75b4ef166d077aada

--- components/Tooltip/Tooltip.tsx
+++ components/Tooltip/Tooltip.tsx
@@ -11,8 +11,7 @@ export const Tooltip: FC<Partial<Props> & Pick<Props, 'tooltipId'>> = ({ tooltip
     <ReactTooltip
       id={ tooltipId }
       className={ `bg-brand-400 text-base-50 font-light text-base
-        rounded-md py-1.5 px-2.5` }
-      opacity={ 1 }
+        rounded-md py-1.5 px-2.5 opacity-100` }
       place={ place }
     />
   )

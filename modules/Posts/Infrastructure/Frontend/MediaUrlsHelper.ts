5ad9e20fdb7de95cb9f1ed30a4d566bded90b8fe
Carlos Ponton <carlitosponton@hotmail.com>
2025-01-20 16:45:26 +0100

    Change providers order
    Former-commit-id: cefd0938ed93b48daea848ada5511bd9f1567e46

--- modules/Posts/Infrastructure/Frontend/MediaUrlsHelper.ts
+++ modules/Posts/Infrastructure/Frontend/MediaUrlsHelper.ts
@@ -101,6 +101,7 @@ export class MediaUrlsHelper {
     download = false
   ): MediaUrlComponentDto[] {
     const providersAccessOrder = [
+      { id: 'ab535237-2262-4763-2443-dfec1a6ec1b9' }, // Vidhide
       { id: '9a51b189-0cbe-4c68-822a-440b61301ec0' }, // Direct
       { id: '8dfb8312-ab59-49df-8047-3a3413512c10' }, // Upnshare
       { id: '35677ea5-3641-4319-ae6f-1fe145c9b797' }, // Lulustream
@@ -109,7 +110,6 @@ export class MediaUrlsHelper {
       { id: '70802e86-7a26-463b-8036-81d7d4ceac1a' }, // Voe
       { id: 'baa39748-8402-4378-b296-3ca653e97f9a' }, // Doodstream
       { id: '9327efd9-4ac5-4ddc-a7e0-bae8c46eb189' }, // Vidguard
-      { id: 'ab535237-2262-4763-2443-dfec1a6ec1b9' }, // Vidhide
       { id: '2fef1a77-a1f8-42a3-9293-437a6f4fc5cc' }, // Wolfstream
       { id: 'd460ba6c-5b2e-4cc9-a09e-eda5d5a7e12c' }, // Fastream
     ]
@@ -117,13 +117,13 @@ export class MediaUrlsHelper {
     const providersDownloadOrder = [
       { id: '9a51b189-0cbe-4c68-822a-440b61301ec0' }, // Direct
       { id: 'e0f92228-068c-43a5-a61f-b7262f124868' }, // Katfile
+      { id: 'ab535237-2262-4763-2443-dfec1a6ec1b9' }, // Vidhide
       { id: '3810a3c5-e4d1-4c99-bee0-7b611aafc89b' }, // 1fichier
       { id: 'b06ba1c1-4988-4b90-8271-a23a56fb0f61' }, // Filemoon
       { id: '9327efd9-4ac5-4ddc-a7e0-bae8c46eb189' }, // Vidguard
       { id: '70802e86-7a26-463b-8036-81d7d4ceac1a' }, // Voe
       { id: 'baa39748-8402-4378-b296-3ca653e97f9a' }, // Doodstream
       { id: '6a594991-7364-481d-85f1-62c5ba2b6cb3' }, // Vtube
-      { id: 'ab535237-2262-4763-2443-dfec1a6ec1b9' }, // Vidhide
       { id: '2fef1a77-a1f8-42a3-9293-437a6f4fc5cc' }, // Wolfstream
       { id: 'd460ba6c-5b2e-4cc9-a09e-eda5d5a7e12c' }, // Fastream
       { id: '35677ea5-3641-4319-ae6f-1fe145c9b797' }, // Lulustream
